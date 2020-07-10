b32a0953475cb36f14c823fac4c8deaca7a7898a
Jake <36495719+JakeC007@users.noreply.github.com>
2020-07-10 16:14:01 -0400

    Update mergeDOI.py

--- mergeDOI.py
+++ mergeDOI.py
@@ -3,14 +3,15 @@ import re
 def main():
   outputlst = []
   doiDict = {}
-
-  
+ 
   #read from file
   fInput = open('input.txt', 'r')
   lines = fInput.read().splitlines()
   fInput.close()
 
   for line in lines:
+    if line == "": #skip new char chars
+      continue
     chunkACM = re.search('.ca/(.*)', line) #search for the doi ACM style
     if chunkACM:
       doi = chunkACM.group(1)
@@ -18,12 +19,10 @@ def main():
       chunkIEEE = re.search('doi: (.*) URL', line)
       if chunkIEEE:
         doi = chunkIEEE.group(1)
-        print("IEEE found")
+    doi = doi.strip()
     print(doi)
+    doiDict[doi]= doiDict.setdefault(doi, 0) + 1
   
+  print(sorted(doiDict.items(), key=lambda x: x[1]))
   
-  
-
-
-
 main()
