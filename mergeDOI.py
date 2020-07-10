a6bdb2ad3d3d12cf9e23ebed21806aa43866303d
Jake <36495719+JakeC007@users.noreply.github.com>
2020-07-10 16:32:03 -0400

    Update mergeDOI.py

--- mergeDOI.py
+++ mergeDOI.py
@@ -10,19 +10,21 @@ def main():
   fInput.close()
 
   for line in lines:
+    doi = "" #for extra protection
     if line == "": #skip new char chars
       continue
     chunkACM = re.search('.ca/(.*)', line) #search for the doi ACM style
     if chunkACM:
       doi = chunkACM.group(1)
     else: #it must be IEEE style  
-      chunkIEEE = re.search('doi: (.*) URL', line)
+      chunkIEEE = re.search('doi: (.*)URL', line)
       if chunkIEEE:
         doi = chunkIEEE.group(1)
     doi = doi.strip()
     print(doi)
     doiDict[doi]= doiDict.setdefault(doi, 0) + 1
   
+  print("There are %d number of citations" %(len(doiDict)))
   print(sorted(doiDict.items(), key=lambda x: x[1]))
   
 main()
