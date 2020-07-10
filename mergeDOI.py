7ec59639637965106d51e852dee7d8f126066d49
Remote-Jake <gchanen1@swarthmore.edu>
2020-07-10 19:58:36 -0400

    refactored code

--- mergeDOI.py
+++ mergeDOI.py
@@ -1,34 +1,42 @@
 import re
 import xlrd
+import time
+from collections import defaultdict
 
 def main():
-  global doiDict
-  doiDict= {}
-  wb = xlrd.open_workbook("RAISE_NO_MERGE.xlsx")
-  delLST = []
+    global doiDict, locDict
+    doiDict= {}
+    locDict = defaultdict(list) #TODO FINISH BELOW
+    wb = xlrd.open_workbook("RAISE_NO_MERGE.xlsx")
+    delLST = []
 
 
-  #loop through the three sheets
-  for i in range(1): #TODO SUB IN wb.nsheets-1
-    sheet = wb.sheet_by_index(11)
-    for col in range(1,3): # cols B & C
-        for row in range(4, sheet.nrows):
-            cell = sheet.cell_value(row, col)
-            del_flag = extractDOI(cell)
-            if del_flag: #cell is dup, grab for deletion lst
-                delLST.append([sheet, row, col])
+    #loop through the three sheets
+    for i in range(1): #TODO SUB IN wb.nsheets-1
+        sheet = wb.sheet_by_index(11)
+        for col in range(1,3): # cols B & C
+            for row in range(4, sheet.nrows):
+                cell = sheet.cell_value(row, col)
+                del_flag = extractDOI(cell, sheet, row, col)
+                #cell is dup, grab for deletion lst
+                if del_flag:
+                    delLST.append([sheet, row, col])
+
+    print("There are %d citations" %(len(doiDict)))
+    print("There are %d duplicates" %(len(delLST)))
+    print(delLST)
+"""
+    for tup in delLST:
+        sheet, row, col = tup
+        cell = sheet.cell_value(row, col)
+        print("----------------------")
+        print(cell)
+        print("----------------------")
 
 
-  for tup in delLST:
-      sheet, row, col = tup
-      cell = sheet.cell_value(row, col)
-      print("----------------------")
-      print(cell)
-      print("----------------------")
-
-  print("There are %d number of citations" %(len(doiDict)))
-  print(sorted(doiDict.items(), key=lambda x: x[1]))
-  print(delLST)
+        print(sorted(doiDict.items(), key=lambda x: x[1]))
+        print(delLST)
+"""
 
 
 """
@@ -36,25 +44,34 @@ This fcn uses regex to grab the doi number and add it to the global hashtable
 
 @param: cell with citation data
 @ret:
-    - True if doi is already in hashtable
-    - False if doi is new
+- True if doi is already in hashtable
+- False if doi is new
 """
-def extractDOI(cell):
-    doi = ""
+def extractDOI(cell, sheet, row, col):
     if cell == "":
         return False
+    print(cell)
+
     chunkACM = re.search('.ca/(.*)', cell) #search for the doi ACM style
     if chunkACM:
-      doi = chunkACM.group(1)
-    else: #it must be IEEE style
-      chunkIEEE = re.search('doi: (.*)URL', cell)
-      if chunkIEEE:
-        doi = chunkIEEE.group(1)
+        doi = chunkACM.group(1)
+    else:
+        chunkIEEE = re.search('doi: (.*)URL', cell)
+        if chunkIEEE:  #it must be IEEE style
+            doi = chunkIEEE.group(1)
+        else:
+            print("-----------------------------------------")
+            print("NO DOI IN IEE OR ACM")
+            print(cell)
+            print("-------------------------------------------")
+            time.sleep(5)
+            doi = cell[0:10]
+
     doi = doi.strip()
-    global doiDict
-    doiDict[doi]= doiDict.setdefault(doi, 0) + 1
+    global doiDict, locDict
+    doiDict[doi] = doiDict.setdefault(doi, 0) + 1
+    locDict[doi].append([sheet, row, col])
     if doiDict[doi] > 1:
-        print(doi)
         return True # this doi is a duplicate
     else:
         return False
