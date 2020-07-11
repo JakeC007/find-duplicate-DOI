0fc434bf2d4865695ed2b0adaaed005a54371b49
Remote-Jake <gchanen1@swarthmore.edu>
2020-07-10 20:45:53 -0400

    finished :star2:

--- mergeDOI.py
+++ mergeDOI.py
@@ -1,47 +1,73 @@
 import re
-import xlrd
+import xlrd, xlwt, xlutils
 import time
+
 from collections import defaultdict
+from xlutils.copy import copy
 
 def main():
     global doiDict, locDict
     doiDict= {}
     locDict = defaultdict(list) #TODO FINISH BELOW
     wb = xlrd.open_workbook("RAISE_NO_MERGE.xlsx")
+    sb = copy(wb) #sb is the excell sheet to save
     delLST = []
 
 
     #loop through the three sheets
-    for i in range(1): #TODO SUB IN wb.nsheets-1
-        sheet = wb.sheet_by_index(11)
+    for i in range(wb.nsheets-1): #TODO SUB IN wb.nsheets-1
+        sheet = wb.sheet_by_index(i)
         for col in range(1,3): # cols B & C
             for row in range(4, sheet.nrows):
                 cell = sheet.cell_value(row, col)
-                del_flag = extractDOI(cell, sheet, row, col)
+                del_flag = extractDOI(cell, i, row, col)
                 #cell is dup, grab for deletion lst
                 if del_flag:
-                    delLST.append([sheet, row, col])
+                    delLST.append([i, row, col])
 
     print("There are %d citations" %(len(doiDict)))
     print("There are %d duplicates" %(len(delLST)))
-    print(delLST)
+    print(sorted(doiDict.items(), key=lambda x: x[1]))
+    # print(delLST)
+    print("List of Duplicates:")
+    for entry in delLST:
+        index, row, col = entry
+        printCell(wb, index, row, col)
+        deleteCell(sb, index, row, col)
+
 """
-    for tup in delLST:
-        sheet, row, col = tup
-        cell = sheet.cell_value(row, col)
-        print("----------------------")
-        print(cell)
-        print("----------------------")
+delets a cell
+@param: index - the sheet's index in the excell file. Sheet 1 is index = 0
+@param: row
+@param: col
+@param: sb - the write to workbook
+"""
+def deleteCell(sb, index, row, col):
+    sheet = sb.get_sheet(index)
+    sheet.write(row, col, "overwritten")
+    sb.save('RAISE_MERGED.xlsx')
 
 
-        print(sorted(doiDict.items(), key=lambda x: x[1]))
-        print(delLST)
 """
+prints out a cell and the sheet it is from
+@param: index - the sheet's index in the excell file. Sheet 1 is index = 0
+@param: row
+@param: col
+@param: wb - read from the workbook
+@return: NONE
+"""
+def printCell(wb, index, row, col):
+    sheet = wb.sheet_by_index(index)
+    cell = sheet.cell_value(row, col)
+    sheetName = wb.sheet_names()
+    print("Sheet Title: %s\n" %(sheetName[index]))
+    print(cell)
+    print("-------------------------------------\n")
+
 
 
 """
 This fcn uses regex to grab the doi number and add it to the global hashtable
-
 @param: cell with citation data
 @ret:
 - True if doi is already in hashtable
@@ -50,7 +76,7 @@ This fcn uses regex to grab the doi number and add it to the global hashtable
 def extractDOI(cell, sheet, row, col):
     if cell == "":
         return False
-    print(cell)
+    #print(cell)
 
     chunkACM = re.search('.ca/(.*)', cell) #search for the doi ACM style
     if chunkACM:
@@ -60,11 +86,11 @@ def extractDOI(cell, sheet, row, col):
         if chunkIEEE:  #it must be IEEE style
             doi = chunkIEEE.group(1)
         else:
-            print("-----------------------------------------")
-            print("NO DOI IN IEE OR ACM")
-            print(cell)
-            print("-------------------------------------------")
-            time.sleep(5)
+            # print("-----------------------------------------")
+            # print("NO DOI IN IEE OR ACM")
+            # print(cell)
+            # print("-------------------------------------------")
+            # time.sleep(5)
             doi = cell[0:10]
 
     doi = doi.strip()
